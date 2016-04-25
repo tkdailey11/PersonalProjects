1745ff961c059fd95d607d2be554c6b72bb8d724
tkdailey11 <tkdailey11@gmail.com>
2016-04-25 11:38:43 -0600

    Added to matching window

--- StudyAid/MatchingWindow/Form1.cs
+++ StudyAid/MatchingWindow/Form1.cs
@@ -12,9 +12,17 @@ namespace MatchingWindow
 {
     public partial class Form1 : Form
     {
+
+        private TextBox[] Letters;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void changeFontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog1.ShowDialog();
         }
     }
 }
